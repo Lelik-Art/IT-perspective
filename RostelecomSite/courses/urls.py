f1a57c53740318b2c8a79ccee080d5ffd6f31815
wrbzrare <poddanila505@gmail.com>
2025-05-14 01:25:47 +0900

    Delete RostelecomSite directory

--- RostelecomSite/courses/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import get_published_courses, my_courses, enroll_to_course, course_info_view
-
-urlpatterns = [
-    path('', get_published_courses, name='get_published_courses'),
-    path('student-room', my_courses, name='my_courses'),
-    path('enroll', enroll_to_course, name='enroll_to_course'),
-    path('course-info', course_info_view, name='course_info'),
-]
